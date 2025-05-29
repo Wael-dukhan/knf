89e8389b21a5b1e59b0c206982096eb0413b0fef
Wael-dukhan <wael1141998@gmail.com>
2025-05-29 21:46:31 +0300

    update few strings in edit school

--- resources/views/admin/schools/edit.blade.php
+++ resources/views/admin/schools/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app2')
 
-@section('title', __('Edit School'))
+@section('title', __('messages.edit_school'))
 
 @section('content')
     <div class="page-wrapper">
@@ -9,7 +9,7 @@
             <div class="page-header">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="page-title">{{ __('Edit School') }}</h3>
+                        <h3 class="page-title">{{ __('messages.edit_school') }}</h3>
                     </div>
                 </div>
             </div>
@@ -22,24 +22,24 @@
                         @method('PUT')
 
                         <div class="form-group">
-                            <label for="name">{{ __('School Name') }}</label>
+                            <label for="name">{{ __('messages.school_name') }}</label>
                             <input type="text" name="name" class="form-control" id="name"
                                    value="{{ old('name', $school->name) }}" required>
                         </div>
 
                         <div class="form-group">
-                            <label for="location">{{ __('Location') }}</label>
+                            <label for="location">{{ __('messages.location') }}</label>
                             <input type="text" name="location" class="form-control" id="location"
                                    value="{{ old('location', $school->location) }}" required>
                         </div>
 
                         <div class="mt-4">
-                            <button type="submit" class="btn btn-primary">
-                                <i class="fas fa-save me-1"></i> {{ __('messages.update') }}
-                            </button>
                             <a href="javascript:history.back()" class="btn btn-secondary">
                                 <i class="feather-arrow-left"></i> {{ __('messages.back') }}
                             </a>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-save me-1"></i> {{ __('messages.update') }}
+                            </button>
                         </div>
                     </form>
                 </div>
